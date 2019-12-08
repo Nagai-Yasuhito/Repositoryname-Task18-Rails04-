05a30f57cea0c60ad223fc3bdd34f736c3931b9b
Nagai Yasuhito <bosto5gj8@yahoo.co.jp>
2019-12-08 07:14:29 +0000

    Rails07_Question

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
+  validates :password, presence: true
   
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
 #パスワードの文字数制限を追加する
@@ -17,4 +18,7 @@ class User < ApplicationRecord
 
   has_secure_password
   
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
 end
